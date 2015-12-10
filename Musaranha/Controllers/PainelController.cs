ae1973a6379f4a140d1beafbf0f37a9d2842da3a
felipemfp <felipemfpontes@gmail.com>
2015-12-10 08:21:45 -0300

    Inicia protótipo de Painel

---
+++ Musaranha/Controllers/PainelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Musaranha.Controllers
+{
+    public class PainelController : Controller
+    {
+        // GET: Painel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
