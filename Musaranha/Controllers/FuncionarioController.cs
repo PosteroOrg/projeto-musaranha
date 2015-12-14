e16495d611b410f4e6ff660dd999bf70875f506a
chicobentojr <francisco.bento.jr@hotmail.com>
2015-12-13 22:26:27 -0300

    Inicia Gerenciamento de Funcionários
    +Inicia estrutura para inserção de funcionário

---
+++ Musaranha/Controllers/FuncionarioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Musaranha.Controllers
+{
+    [Filters.AutenticacaoFilter]
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Musaranha/Scripts/modules/musaranha.funcionario.js
+++ Musaranha/Scripts/modules/musaranha.funcionario.js
@@ -1,6 +1,17 @@
 ﻿Musaranha.Funcionario = Musaranha.Funcionario || (function () {
     function inciar() {
+        $('.cadastrar.button').click(function () {
+            abrirDialogCadastro();
+        });
+        alert('carregou');
+    }
 
+    function abrirDialogCadastro() {
+        var $dialog = $('[data-role=dialog]');
+
+        $dialog.find('h1').text('Cadastrar Funcionário');
+
+        $dialog.open();
     }
 
     return {
