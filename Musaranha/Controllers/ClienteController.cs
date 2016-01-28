619e10b8f3d645ec2557023716cf5067392cf8f7
felipemfp <felipemfpontes@gmail.com>
2016-01-27 23:52:39 -0300

    Implementa Gerenciamento de Clientes
    - [x] Incluir
    - [x] Listagem
    - [x] Editar
    - [x] Excluir
    - [x] Validadores

--- Musaranha/Controllers/ClienteController.cs
+++ Musaranha/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Musaranha.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,139 @@ namespace Musaranha.Controllers
 {
     public class ClienteController : Controller
     {
-        // GET: Cliente
-        public ActionResult Index() => View();
+        // GET: cliente
+        public ActionResult Index() => View(Cliente.Listar());
+
+        // POST: cliente/incluir
+        [HttpPost]
+        public ActionResult Incluir(FormCollection form)
+        {
+            if (form.HasKeys())
+            {
+                Cliente cliente = new Cliente();
+
+                /* Dados Pessoais */
+                cliente.Pessoa = new Pessoa();
+                cliente.Pessoa.Tipo = form["txtTipo"] ?? "N";
+                cliente.Pessoa.Nome = form["txtNome"];
+                cliente.Pessoa.Telefone.Add(new Telefone { NumTelefone = form["txtTelefone"] });
+                switch (cliente.Pessoa.Tipo)
+                {
+                    case "F":
+                        cliente.Pessoa.CPF = form["txtCPFOuCNPJ"] ?? null;
+                        break;
+                    case "J":
+                        cliente.Pessoa.CNPJ = form["txtCPFOuCNPJ"] ?? null;
+                        break;
+                    default:
+                        break;
+                }
+                cliente.Pessoa.Email = form["txtEmail"] ?? null;
+
+                /* Endereço */
+                if (!String.IsNullOrWhiteSpace(form["txtLogradouro"]))
+                {
+                    cliente.Pessoa.Endereco = new Endereco();
+                    cliente.Pessoa.Endereco.Logradouro = form["txtLogradouro"];
+                    cliente.Pessoa.Endereco.Numero = form["txtNumero"];
+                    cliente.Pessoa.Endereco.Complemento = form["txtComplemento"];
+                    cliente.Pessoa.Endereco.Bairro = form["txtBairro"];
+                    cliente.Pessoa.Endereco.Cidade = form["txtCidade"];
+                    cliente.Pessoa.Endereco.Estado = form["txtEstado"];
+                    cliente.Pessoa.Endereco.CEP = form["txtCEP"];
+                }
+
+                Cliente.Incluir(cliente);
+
+                return PartialView("_Lista", Cliente.Listar());
+            }
+            return Json(false);
+        }
+
+        // POST: cliente/editar/5
+        [HttpPost]
+        public ActionResult Editar(int cod, FormCollection form)
+        {
+            if (cod > 0 && form.HasKeys())
+            {
+                Cliente cliente = Cliente.ObterPorCodigo(cod);
+
+                /* Dados Pessoais */
+                cliente.Pessoa.Tipo = form["txtTipo"] ?? "N";
+                cliente.Pessoa.Nome = form["txtNome"];
+                cliente.Pessoa.Telefone.Clear();
+                cliente.Pessoa.Telefone.Add(new Telefone { NumTelefone = form["txtTelefone"] });
+                switch (cliente.Pessoa.Tipo)
+                {
+                    case "F":
+                        cliente.Pessoa.CPF = form["txtCPFOuCNPJ"] ?? null;
+                        cliente.Pessoa.CNPJ = null;
+                        break;
+                    case "J":
+                        cliente.Pessoa.CNPJ = form["txtCPFOuCNPJ"] ?? null;
+                        cliente.Pessoa.CPF = null;
+                        break;
+                    default:
+                        break;
+                }
+                cliente.Pessoa.Email = form["txtEmail"] ?? null;
+
+                /* Endereço */
+                if (!String.IsNullOrWhiteSpace(form["txtLogradouro"]))
+                {
+                    cliente.Pessoa.Endereco = new Endereco();
+                    cliente.Pessoa.Endereco.Logradouro = form["txtLogradouro"];
+                    cliente.Pessoa.Endereco.Numero = form["txtNumero"];
+                    cliente.Pessoa.Endereco.Complemento = form["txtComplemento"];
+                    cliente.Pessoa.Endereco.Bairro = form["txtBairro"];
+                    cliente.Pessoa.Endereco.Cidade = form["txtCidade"];
+                    cliente.Pessoa.Endereco.Estado = form["txtEstado"];
+                    cliente.Pessoa.Endereco.CEP = form["txtCEP"];
+                }
+
+                Cliente.Editar(cliente);
+
+                return PartialView("_Lista", Cliente.Listar());
+            }
+            return Json(false);
+        }
+
+        // POST: cliente/excluir/5
+        [HttpPost]
+        public ActionResult Excluir(int cod)
+        {
+            if (cod != 0)
+            {
+                Cliente cliente = Cliente.ObterPorCodigo(cod);
+
+                Cliente.Excluir(cliente);
+
+                return PartialView("_Lista", Cliente.Listar());
+            }
+            return Json(false);
+        }
+
+        // POST: cliente/json/5
+        [HttpPost]
+        public ActionResult Json(int cod)
+        {
+            Cliente cliente = Cliente.ObterPorCodigo(cod);
+            return Json(new
+            {
+                Nome = cliente.Pessoa.Nome,
+                Telefone = cliente.Pessoa.Telefone.First()?.NumTelefone,
+                Logradouro = cliente.Pessoa.Endereco?.Logradouro ?? "",
+                Numero = cliente.Pessoa.Endereco?.Numero ?? "",
+                Complemento = cliente.Pessoa.Endereco?.Complemento ?? "",
+                CEP = cliente.Pessoa.Endereco?.CEP ?? "",
+                Bairro = cliente.Pessoa.Endereco?.Bairro ?? "",
+                Cidade = cliente.Pessoa.Endereco?.Cidade ?? "",
+                Estado = cliente.Pessoa.Endereco?.Estado ?? "",
+                Tipo = cliente.Pessoa.Tipo ?? "",
+                CPF = cliente.Pessoa.CPF ?? "",
+                CNPJ = cliente.Pessoa.CNPJ ?? "",
+                Email = cliente.Pessoa.Email ?? ""
+            });
+        }
     }
 }
--- Musaranha/Scripts/modules/musaranha.cliente.js
+++ Musaranha/Scripts/modules/musaranha.cliente.js
@@ -3,16 +3,187 @@
         $('select').material_select();
 
         $('button.incluir').off().click(function () {
-            $('.acao.modal').openModal();
+            abrirModalInclusao();
         });
 
         $('button.editar').off().click(function () {
-            $('.acao.modal').openModal();
+            abrirModalEdicao(this);
         });
 
         $('button.excluir').off().click(function () {
-            $('.excluir.modal').openModal();
+            var $tr = $(this).parents('tr');
+            var codPessoa = $tr.data('cliente');
+            var nome = $tr.find('td').eq(0).text();
+            var telefone = $tr.find('td').eq(1).text();
+            abrirModalExclusao(codPessoa, nome, telefone);
         });
+
+        $('table tr').hover(function () {
+            var $tr = $(this);
+            var $tfoot = $tr.parents('table').find('tfoot');
+            $tfoot.find('[data-campo=endereco]').text($tr.data('endereco'));
+            $tfoot.find('[data-campo=cpfcnpj]').text($tr.data('cnpj') || $tr.data('cpf'));
+            $tfoot.find('[data-campo=email]').text($tr.data('email'));
+        });
+    }
+
+    function abrirModalInclusao() {
+        var $modal = $('.acao.modal');
+        $('select').material_select();
+
+        $modal.find('.header').text('Incluir Cliente');
+        $modal.find('.primary').text('Incluir').off().click(function () {
+            incluir();
+        });
+
+        $modal.openModal();
+    }
+
+    function abrirModalEdicao(button) {
+        var $modal = $('.acao.modal');
+        var $tds = $(button).parents('tr').find('td');
+        var codPessoa = $(button).parents('tr').data('cliente');
+
+        $modal.find('.header').text('Editar Funcionário');
+        
+        $.ajax({
+            url: '/cliente/json/' + codPessoa,
+            type: 'POST',
+            success: function (cliente) {
+                $modal.find('#txtNome').val(cliente.Nome);
+                $modal.find('#txtTelefone').val(cliente.Telefone);
+                $modal.find('#txtLogradouro').val(cliente.Logradouro);
+                $modal.find('#txtNumero').val(cliente.Numero);
+                $modal.find('#txtComplemento').val(cliente.Complemento);
+                $modal.find('#txtCEP').val(cliente.CEP);
+                $modal.find('#txtBairro').val(cliente.Bairro);
+                $modal.find('#txtCidade').val(cliente.Cidade);
+                $modal.find('#txtEstado').val(cliente.Estado);
+                $modal.find('#txtTipo').val(cliente.Tipo).material_select();
+                $modal.find('#txtCPFOuCNPJ').val(cliente.CPF || cliente.CNPJ);
+                $modal.find('#txtEmail').val(cliente.Email);
+
+                $modal.find('.primary').text('Editar').off().click(function () {
+                    editar(codPessoa);
+                });
+
+                $modal.openModal();
+            }
+        });        
+    }
+
+    function abrirModalExclusao(codPessoa, nome, telefone) {
+        var $modal = $('.excluir.modal');
+        $modal.find('.info').html('');
+        $modal.find('.info').append('<p><b>Nome: </b>' + nome + '</p>');
+        $modal.find('.info').append('<p><b>Telefone: </b>' + telefone + '</p>');
+
+        $modal.find('.primary').off().click(function () {
+            excluir(codPessoa);
+        });
+
+        $modal.openModal();
+    }
+
+    function incluir() {
+        if (validar()) {
+            var form = $('form.acao.modal').serializeArray();
+            $('form.acao.modal .modal-footer').append(
+                '<div class="progress">' +
+                  '<div class="indeterminate"></div>' +
+                '</div>');
+            $.ajax({
+                type: 'POST',
+                url: '/cliente/incluir',
+                data: form,
+                success: function (clientes) {
+                    var $tbody = $('.table.clientes tbody');
+                    $tbody.html(clientes);
+                    Materialize.toast('Cliente incluído com sucesso', 4000);
+                    iniciar();
+                },
+                error: function () {
+                    Materialize.toast('Ocorreu um erro na inclusão do Cliente', 4000);
+                },
+                complete: function () {
+                    $('form.acao.modal .modal-footer .progress').remove();
+                    $('form.acao.modal').get(0).reset();
+                    $('form.acao.modal').closeModal();
+                }
+            })
+        }
+        else return false;
+    }
+
+    function editar(codPessoa) {
+        if (validar()) {
+            var form = $('form.acao.modal').serializeArray();
+            $('form.acao.modal .modal-footer').append(
+                '<div class="progress">' +
+                  '<div class="indeterminate"></div>' +
+                '</div>');
+            $.ajax({
+                type: 'POST',
+                data: form,
+                url: '/cliente/editar/' + codPessoa,
+                success: function (clientes) {
+                    var $tbody = $('table tbody');
+                    $tbody.html(clientes);
+                    Materialize.toast('Cliente editado com sucesso', 4000);
+                    iniciar();
+                },
+                error: function () {
+                    Materialize.toast('Ocorreu um erro na edição do Cliente', 4000);
+                },
+                complete: function () {
+                    $('form.acao.modal .modal-footer .progress').remove();
+                    $('form.acao.modal').get(0).reset();
+                    $('form.acao.modal').closeModal();
+                }
+            })
+        }
+        else return false;
+    }
+
+    function excluir(codPessoa) {
+        $('form.excluir.modal .modal-footer').append(
+                '<div class="progress">' +
+                  '<div class="indeterminate"></div>' +
+                '</div>');
+        $.ajax({
+            type: 'POST',
+            url: '/cliente/excluir/' + codPessoa,
+            success: function (clientes) {
+                var $tbody = $('table tbody');
+                $tbody.html(clientes);
+                Materialize.toast('Cliente excluído com sucesso', 4000);
+                iniciar();
+            },
+            error: function () {
+                Materialize.toast('Ocorreu um erro na exclusão do Cliente', 4000);
+            },
+            complete: function () {
+                $('form.excluir.modal .modal-footer .progress').remove();
+                $('form.excluir.modal').find('.info').html('');
+                $('form.excluir.modal').closeModal();
+            }
+        })
+    }
+
+    function validar() {
+        var valido = true;
+        var $form = $('form');
+
+        if (!$('#txtNome').val()) {
+            $('#txtNome').addClass("invalid");
+            valido = false;
+        }
+        if (!$('#txtTelefone').val()) {
+            $('#txtTelefone').addClass("invalid");
+            valido = false;
+        }
+
+        return valido;
     }
 
     return {
