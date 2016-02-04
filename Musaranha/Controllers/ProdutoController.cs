26216888ca2ab372aea2e1ae73f6255f7392e71d
felipemfp <felipemfpontes@gmail.com>
2016-02-03 21:59:02 -0300

    Implementa controller de Produto

--- Musaranha/Controllers/ProdutoController.cs
+++ Musaranha/Controllers/ProdutoController.cs
@@ -3,12 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Musaranha.Models;
 
 namespace Musaranha.Controllers
 {
     public class ProdutoController : Controller
     {
-        // GET: Produto
-        public ActionResult Index() => View();
+        // GET: produto
+        public ActionResult Index() => View(Produto.Listar());
+
+        // POST: produto/incluir
+        [HttpPost]
+        public ActionResult Incluir(FormCollection form)
+        {
+            if (form.HasKeys())
+            {
+                Produto produto = new Produto();
+                produto.Descricao = form["txtDescricao"];
+                Produto.Incluir(produto);
+                return PartialView("_Lista", Produto.Listar());
+            }
+            return Json(false);
+        }
+
+        // POST: produto/editar/5
+        [HttpPost]
+        public ActionResult Editar(int cod, FormCollection form)
+        {
+            if (cod > 0 && form.HasKeys())
+            {
+                Produto produto = Produto.ObterPorCodigo(cod);
+                produto.Descricao = form["txtDescricao"];
+                Produto.Editar(produto);
+                return PartialView("_Lista", Produto.Listar());
+            }
+            return Json(false);
+        }
+
+        // POST: produto/excluir/5
+        [HttpPost]
+        public ActionResult Excluir(int cod)
+        {
+            if (cod != 0)
+            {
+                Produto produto = Produto.ObterPorCodigo(cod);
+                Produto.Excluir(produto);
+                return PartialView("_Lista", Produto.Listar());
+            }
+            return Json(false);
+        }
     }
 }
