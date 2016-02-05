5398fdb852d9088de387ad09a84e399ae4ad9c60
felipemfp <felipemfpontes@gmail.com>
2016-02-05 14:48:33 -0300

    Realiza tradução de forma global

--- Musaranha/Scripts/modules/musaranha.venda.js
+++ Musaranha/Scripts/modules/musaranha.venda.js
@@ -1,20 +1,7 @@
 ﻿Musaranha.Venda = Musaranha.Venda || (function () {
     function iniciar() {
         $('select').material_select();
-        $('.datepicker').pickadate({
-            labelMonthNext: 'Próximo mês',
-            labelMonthPrev: 'Mês anterior',
-            labelMonthSelect: 'Selecione o mês',
-            labelYearSelect: 'Selecione o ano',
-            monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-            monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-            weekdaysFull: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-            weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-            weekdaysLetter: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
-            today: 'Hoje',
-            clear: 'Limpar',
-            close: 'Fechar'
-        });
+        $('.datepicker').pickadate();
         $('.modal-trigger').leanModal();
 
 
