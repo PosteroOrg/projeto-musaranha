5707eab95a7c95fbb1f6bf4507d823d86a160a77
felipemfp <felipemfpontes@gmail.com>
2016-02-05 14:31:08 -0300

    Realiza tradução dos Datepickers

--- Musaranha/Scripts/modules/musaranha.funcionario.js
+++ Musaranha/Scripts/modules/musaranha.funcionario.js
@@ -204,6 +204,18 @@ Musaranha.Funcionario.Pagamento = Musaranha.Funcionario.Pagamento || (function (
     function iniciar() {
         $('select').material_select();
         $('.datepicker').pickadate({
+            labelMonthNext: 'Próximo mês',
+            labelMonthPrev: 'Mês anterior',
+            labelMonthSelect: 'Selecione o mês',
+            labelYearSelect: 'Selecione o ano',
+            monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            weekdaysFull: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+            weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+            weekdaysLetter: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+            today: 'Hoje',
+            clear: 'Limpar',
+            close: 'Fechar',
             selectMonths: true,
             selectYears: true
         });
