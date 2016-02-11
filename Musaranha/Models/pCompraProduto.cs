7b8e92bd0db8a6eb82a0653914a270d7a9d1459e
felipemfp <felipemfpontes@gmail.com>
2016-02-10 23:18:45 -0300

    Implementa gerenciamento de Compras
    - [x] Incluir
    - [x] Editar
    - [x] Excluir
    - [x] Listar
    - [x] Filtrar

---
+++ Musaranha/Models/pCompraProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Musaranha.Models
+{
+    public partial class CompraProduto
+    {
+        public string QuantidadeUnidade => $"{this.Quantidade} {this.Unidade}";
+        public decimal Valor => this.PrecoUnitario * (decimal)this.Quantidade;
+    }
+}
---
+++ Musaranha/ViewModels/CompraEditarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Musaranha.Models;
+
+namespace Musaranha.ViewModels
+{
+    public class CompraEditarViewModel
+    {
+        public Compra Compra { get; set; }
+        public List<Fornecedor> Fornecedores { get; set; } = new List<Fornecedor>();
+        public List<Produto> Produtos { get; set; } = new List<Produto>();
+    }
+}
